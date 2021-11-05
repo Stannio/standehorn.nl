795ad8ff01756e156081c7b0e7d96ff6dcc2c734
Stan <stannio99@gmail.com>
2021-11-05 22:46:19 +0100

    Add basic about me section

--- components/theme/DarkModeToggle.tsx
+++ components/theme/DarkModeToggle.tsx
@@ -21,28 +21,28 @@ const DarkModeToggle = () => {
     setTheme(option.value);
   };
   return (
-    <div className='w-36'>
+    <div className="w-36">
       <Listbox value={selected} onChange={setDarkMode}>
-        <div className='relative mt-1'>
-          <Listbox.Button className='relative w-full py-2 pl-10 pr-10 text-left text-white bg-gray-900 rounded-lg shadow-md cursor-default dark:bg-white dark:text-black focus:outline-none focus-visible:ring-2 focus-visible:ring-opacity-75 focus-visible:ring-white focus-visible:ring-offset-orange-300 focus-visible:ring-offset-2 focus-visible:border-indigo-500 sm:text-sm'>
-            <span className='absolute inset-y-0 left-0 flex items-center pl-2 pointer-events-none'>
-              <MoonIcon className='w-5 h-5' aria-hidden='true' />
+        <div className="relative mt-1">
+          <Listbox.Button className="relative w-full py-2 pl-10 pr-10 text-left text-white bg-gray-900 rounded-lg shadow-md cursor-default dark:bg-white dark:text-black focus:outline-none focus-visible:ring-2 focus-visible:ring-opacity-75 focus-visible:ring-white focus-visible:ring-offset-orange-300 focus-visible:ring-offset-2 focus-visible:border-indigo-500 sm:text-sm">
+            <span className="absolute inset-y-0 left-0 flex items-center pl-2 pointer-events-none">
+              <MoonIcon className="w-5 h-5" aria-hidden="true" />
             </span>
-            <span className='block truncate'>{selected.name}</span>
-            <span className='absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none'>
+            <span className="block truncate">{selected.name}</span>
+            <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
               <SelectorIcon
-                className='w-5 h-5 text-gray-400'
-                aria-hidden='true'
+                className="w-5 h-5 text-gray-400"
+                aria-hidden="true"
               />
             </span>
           </Listbox.Button>
           <Transition
             as={Fragment}
-            leave='transition ease-in duration-100'
-            leaveFrom='opacity-100'
-            leaveTo='opacity-0'
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
           >
-            <Listbox.Options className='absolute w-full py-1 mt-1 overflow-auto text-base bg-white rounded-md shadow-lg max-h-60 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm'>
+            <Listbox.Options className="absolute w-full py-1 mt-1 overflow-auto text-base bg-white rounded-md shadow-lg bottom-10 max-h-60 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
               {options.map((option, key) => (
                 <Listbox.Option
                   key={key}
@@ -74,7 +74,7 @@ const DarkModeToggle = () => {
                           }
                                 absolute inset-y-0 left-0 flex items-center pl-3`}
                         >
-                          <CheckIcon className='w-5 h-5' aria-hidden='true' />
+                          <CheckIcon className="w-5 h-5" aria-hidden="true" />
                         </span>
                       ) : null}
                     </>
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,30 +7,45 @@ import Footer from "../components/navigation/Footer";
 
 const Home: NextPage = () => {
   return (
-    <div className='h-screen'>
+    <div className="h-screen">
       <Navbar />
-      <div className='container px-1 mx-auto h-4/6'>
-        <div className='grid h-full grid-cols-6'>
-          <div className='col-span-6 lg:col-span-3 xl:col-span-2 xl:col-start-2 place-self-center'>
-            <h1 className='font-mono text-6xl font-semibold leading-tight text-black dark:text-white'>
+      <div className="container px-1 mx-auto h-4/6">
+        <div className="grid h-full grid-cols-6">
+          <div className="col-span-6 lg:col-span-3 xl:col-span-2 xl:col-start-2 place-self-center">
+            <h1 className="font-mono text-6xl font-semibold leading-tight text-black dark:text-white">
               Full-stack developer
             </h1>
-            <p className='my-6 font-mono text-xl leading-tight text-black dark:text-white'>
+            <p className="my-6 font-mono text-xl leading-tight text-black dark:text-white">
               I&#39;m a full-stack developer with a passion for building
               applications.
             </p>
             <Tags />
           </div>
-          <div className='col-span-6 lg:col-span-2 xl:col-span-1 place-self-center'>
-            <div className='w-64 h-auto'>
+          <div className="col-span-6 lg:col-span-2 xl:col-span-1 place-self-center">
+            <div className="w-64 h-auto">
               <Image
                 src={StanImage}
-                className='rounded-full shadow-lg'
-                placeholder='blur'
-                alt='Stan'
-                layout='responsive'
+                className="rounded-full shadow-lg"
+                placeholder="blur"
+                alt="Stan"
+                layout="responsive"
               />
             </div>
+          </div>
+        </div>
+      </div>
+      <div className="container mx-auto">
+        <div className="grid grid-cols-6 gap-8">
+          <div className="self-center col-span-6 lg:col-span-3 xl:col-span-2 xl:col-start-2">
+            <h1 className="font-mono text-6xl font-semibold leading-tight text-black border-b border-gray-700 dark:text-white">
+              About me
+            </h1>
+            <p className="my-6 font-sans font-semibold text-black dark:text-white">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus,
+              et laboriosam. Provident nesciunt a necessitatibus et nulla
+              debitis beatae, saepe laboriosam iure iste, deleniti non qui
+              earum, laborum ullam voluptate.
+            </p>
           </div>
         </div>
       </div>
