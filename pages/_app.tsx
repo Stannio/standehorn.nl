4861ce082bf64e94118007f83fd144cbb6a1aa46
Stannio <stannio99@gmail.com>
2021-11-05 15:51:07 +0100

    Add next-seo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,22 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
+import { NextSeo } from "next-seo";
 
 function PortfolioApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute='class' defaultTheme='system'>
+      <NextSeo
+        title='Portfolio • Stan'
+        description={`A showcase of Stan's portfolio`}
+        canonical='https://standehorn.nl/'
+        openGraph={{
+          url: "https://standehorn.nl",
+          title: "Portfolio • Stan",
+          description: "A showcase of Stan's portfolio",
+          site_name: "Portfolio",
+        }}
+      />
       <Component {...pageProps} />
     </ThemeProvider>
   );
